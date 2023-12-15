d3bc4e5e7bbfb08b1de7e5234bfa4d7641ae0516
Dani <perdan90@gmail.com>
2023-12-15 09:52:15 +0100

    refactor(Yatzy): simplify smallStraight algorithm

--- src/main/java/cd/dperez/Yatzy.java
+++ src/main/java/cd/dperez/Yatzy.java
@@ -97,20 +97,11 @@ public class Yatzy {
         return 0;
     }
 
-    public static int smallStraight(int d1, int d2, int d3, int d4, int d5)
-    {
-        int[] tallies;
-        tallies = new int[6];
-        tallies[d1-1] += 1;
-        tallies[d2-1] += 1;
-        tallies[d3-1] += 1;
-        tallies[d4-1] += 1;
-        tallies[d5-1] += 1;
-        if (tallies[0] == 1 &&
-                tallies[1] == 1 &&
-                tallies[2] == 1 &&
-                tallies[3] == 1 &&
-                tallies[4] == 1)
+    public static int smallStraight(int... dice) {
+        final int[] SMALL_STRAIGHT = {1, 2, 3, 4, 5};
+
+        Arrays.sort(dice);
+        if (Arrays.equals(dice, SMALL_STRAIGHT))
             return 15;
         return 0;
     }
