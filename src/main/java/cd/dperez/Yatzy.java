e17831b966b50b4bb47dbfc1478896493060fced
Dani <perdan90@gmail.com>
2023-12-15 05:38:38 +0100

    refactor(Yatzy): change long parameter list to varargs and simplified sum with stream Change the five int parameters for a varargs parameter, and change all the unnecesary repetaed sums with a stream.sum to simplify code and avoid errors.

--- src/main/java/cd/dperez/Yatzy.java
+++ src/main/java/cd/dperez/Yatzy.java
@@ -1,16 +1,11 @@
 package cd.dperez;
 
+import java.util.Arrays;
+
 public class Yatzy {
 
-    public static int chance(int d1, int d2, int d3, int d4, int d5)
-    {
-        int total = 0;
-        total += d1;
-        total += d2;
-        total += d3;
-        total += d4;
-        total += d5;
-        return total;
+    public static int chance(int... dice) {
+       return Arrays.stream(dice).sum();
     }
 
     public static int yatzy(int... dice)
